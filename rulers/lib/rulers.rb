881d426e5d8e8a3e8c6b7d9ced0696c45070ad90
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-04 22:12:41 -0800

    more redirects/codes for particular links

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -6,13 +6,22 @@ module Rulers
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        return [404, {'Content-Type' => 'text/html'},
+                ["A root page is not set yet"]]
       end
 
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
-          [text]]
+      begin
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+      rescue NoMethodError
+        [501, {'Content-Type' => 'text/html'},
+                ["Nice try, sukka! Route not implemented."]]
+      else
+        text = controller.send(act)
+        [200, {'Content-Type' => 'text/html'},
+            [text]]
+      end
     end
   end
 
