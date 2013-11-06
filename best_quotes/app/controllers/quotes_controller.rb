ee361e3199ac1b47cb9fd5d9293b1a8a3184af79
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-06 15:14:18 -0800

    index method and view for quotes

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,4 +1,23 @@
 class QuotesController < Rulers::Controller
+  def index
+    @quotes = FileModel.all
+    render :index
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Jo-mama"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def update
+    # TODO: Write an update POST method.
+  end
+
   def a_quote
     @verb = "crying"
     render :a_quote
