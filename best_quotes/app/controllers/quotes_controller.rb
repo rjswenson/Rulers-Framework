bc58ca7664b0d954bd7518b3d7d28259fb3e8d7d
Robin Swenson-Healey <rjswenson@gmail.com>
2013-11-06 13:01:19 -0800

    controller passing variables to views

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -7,4 +7,9 @@ class QuotesController < Rulers::Controller
   def exception
     raise "It's a bad one!"
   end
+
+  def quote_1
+    quote_1 = Rulers::Model::FindModel.find(1)
+    render :quote, :obj => quote_1
+  end
  end
